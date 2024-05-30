ffbfa08420b84037d4ccb3926ae7d5acb620d052
michalkossakowski <141955539+michalkossakowski@users.noreply.github.com>
2024-05-30 20:04:00 +0200

    system tur
    system tur

---
+++ Assets/Scripts/HudScripts/Sleeping.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sleeping : MonoBehaviour
+{
+    public string nextLevel = "HomeLozko";
+    public int levelTime = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Timer.SetTime(levelTime);
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
