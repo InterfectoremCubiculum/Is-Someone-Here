9a980681b1a02dbc980f7f0ac87d594e1fc10d28
Mateusz Izdebski <mateusz.izdebski125@gmail.com>
2024-05-30 18:10:24 +0200

    poprawka do speeda
    .

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed= 0.05f;
+    public float runSpeed = 0.2f;
+    public int speedTime = 2;
+    float speedTimeLeft;
     Transform orientation;
     float horizontalInput;
     float verticalInput;
@@ -15,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
     {
         eyesCamera = Camera.main;
         orientation = eyesCamera.transform;
+        speedTimeLeft = speedTime;
     }
 
     // Update is called once per frame
@@ -35,9 +39,17 @@ public class PlayerMovement : MonoBehaviour
     }
     private void MovePlayer()
     {
-
-            moveDirection = verticalInput* orientation.forward * moveSpeed + horizontalInput*orientation.right * moveSpeed;
+        if (Input.GetKey(KeyCode.LeftShift) && speedTimeLeft>0)
+        {
+            moveDirection = verticalInput * orientation.forward * runSpeed + horizontalInput * orientation.right * runSpeed;
             transform.position += new Vector3(moveDirection.x, 0f, moveDirection.z);
+            speedTimeLeft -= Time.deltaTime;
+        }
+        else
+        {
+            moveDirection = verticalInput * orientation.forward * moveSpeed + horizontalInput * orientation.right * moveSpeed;
+            transform.position += new Vector3(moveDirection.x, 0f, moveDirection.z);
+        }
     }
     
 }
