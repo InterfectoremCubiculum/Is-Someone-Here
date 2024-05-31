6bb14f05b2a20bde069fc73ca108260c10e46e8e
Kex <krzysieksobol9@gmail.com>
2024-05-31 13:34:04 +0200

    LightSwitch script added

--- Assets/Scripts/GameControllScripts/LightSwitchScript.cs
+++ Assets/Scripts/GameControllScripts/LightSwitchScript.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LightSwitchScript : MonoBehaviour
 {
+    private Renderer _renderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _renderer = GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Check for mouse click
+        if (Input.GetMouseButtonDown(0)) // 0 is for left mouse button, you can change to other buttons if needed
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("Switch"))
+                {
+                    Debug.Log("Clicked a switch");
+                    // Add any additional logic you want to execute when the switch is clicked
+                }
+            }
+        }
     }
 }
