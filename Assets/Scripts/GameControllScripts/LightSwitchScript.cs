9f47ce0fab2b0651220464877f5c0fc83242556e
Kex <krzysieksobol9@gmail.com>
2024-05-31 14:55:07 +0200

    LightSwitchScript works on cubes
    add a script to the cube
    and then choose a lightswitch name that will be changes

--- Assets/Scripts/GameControllScripts/LightSwitchScript.cs
+++ Assets/Scripts/GameControllScripts/LightSwitchScript.cs
@@ -8,6 +8,7 @@ public class LightSwitchScript : MonoBehaviour
     private Transform player;
     private RaycastHit raycastHit;
     public float maxSelectionRange;
+    public string lightObjectName; // Public variable for the name of the light object
 
     void Start()
     {
@@ -30,6 +31,25 @@ public class LightSwitchScript : MonoBehaviour
                 if (Input.GetKeyDown("e"))
                 {
                     Debug.Log("Selected: " + target.name);
+                    
+                    // Find the light object by name
+                    GameObject lightObject = GameObject.Find(lightObjectName);
+                    if (lightObject != null)
+                    {
+                        Light light = lightObject.GetComponent<Light>();
+                        if (light != null)
+                        {
+                            light.enabled = !light.enabled;
+                        }
+                        else
+                        {
+                            Debug.LogWarning("No Light component found on the specified light object.");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogWarning("No GameObject found with the specified light object name.");
+                    }
                 }
             }
         }
